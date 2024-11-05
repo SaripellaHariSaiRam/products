2869950b5b8ee82097878a1af1b21f8e0b113634
SaripellaHariSaiRam <sairambunny845@gmail.com>
2024-11-05 16:05:14 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express")
+
+const app = express()
+
+app.get("/products", (req, res)=>{
+    res.send("Hello")
+})
+
+app.get("/users", (req, res)=>{
+    res.send("bye byee")
+})
+
+
+app.listen(3000, ()=>{
+    console.log("listening to the port")
+})
